943ae3eef3241ee2c8f9b41e08765945d907ff0c
Quacktit <batak.2quack@gmail.com>
2026-05-06 00:27:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//----------------The COMEDY FACTORY----------------//
+
+
+
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        if(this.getAttribute('href').startsWith("#")) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href'))
+                .scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
